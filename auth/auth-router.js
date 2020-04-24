4e6b37cbb0d89a8e6cecedd6276e0e9ccc4b475d
Miranda Musselman <mirandamusselman@Pams-MacBook-Pro.local>
2020-04-24 09:46:52 -0600

    create endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../users/model.js");
+const secrets = require("../api/secrets.js");
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 8;
+  const hash = bcrypt.hashSync(user.password, rounds);
+  user.password = hash;
+
+  Users.add(user)
+       .then(reg => {
+         res.status(201).json(reg)
+       })
+       .catch(error => {
+         console.log(error);
+         res.status(500).json({ errorMessage: error.message });
+       })
 });
 
 router.post('/login', (req, res) => {
